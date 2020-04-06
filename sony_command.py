e57a4ceef2032f3e4f0a83eba3f76c21a1f64851
Riseryn <56023588+Riseryn@users.noreply.github.com>
2020-04-06 19:28:27 +0200

    Update sony_command.py

--- sony_command.py
+++ sony_command.py
@@ -123,7 +123,7 @@ class Sony_command(NeuronModule):
             dat +='"version" : "1.2"}'
         else:
             dat +='"version" : "1.0"}'
-        #print("Data= ",dat)
+
         return head,dat
 
     #   execute the request
